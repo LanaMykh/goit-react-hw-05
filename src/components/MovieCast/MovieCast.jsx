222771ee7d17a6067cf5d5e8284eb306317589bc
Svitlana Mykhailiuk <svetiklan@gmail.com>
2024-11-14 15:40:54 +0200

    add info

--- src/components/MovieCast/MovieCast.jsx
+++ src/components/MovieCast/MovieCast.jsx
@@ -1,5 +1,61 @@
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { getMovieCast, imgBaseURL } from '../../movies-api';
+import styles from './MovieCast.module.css';
+
 const MovieCast = () => {
-  return <div>MovieCast</div>;
+  const [movieCasts, setMovieCasts] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState('');
+
+  const { movieId } = useParams();
+
+  useEffect(() => {
+    if (!movieId) return;
+
+    async function fetchMovieCastById(movieId) {
+      try {
+        setError('');
+        setIsLoading(true);
+        const { cast } = await getMovieCast(movieId);
+        setMovieCasts(cast);
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+
+    fetchMovieCastById(movieId);
+  }, [movieId]);
+
+  return (
+    <div>
+      {error && <p>{error}</p>}
+      {isLoading && <p>Wait a few moments. Loading...</p>}
+
+      <ul>
+        {movieCasts.map(actor => {
+          return (
+            <li key={actor.id}>
+              {actor.profile_path ? (
+                <img
+                  src={`${imgBaseURL}${actor.profile_path}`}
+                  alt={actor.name}
+                  width={100}
+                />
+              ) : (
+                <span className={styles.noFoto}>X</span>
+              )}
+
+              <h3>{actor.name}</h3>
+              <p>Character: {actor.character}</p>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
 };
 
 export default MovieCast;
--- src/components/MovieReviews/MovieReviews.jsx
+++ src/components/MovieReviews/MovieReviews.jsx
@@ -1,5 +1,50 @@
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { getMovieReviews } from '../../movies-api';
+
 const MovieReviews = () => {
-  return <div>MovieReviews</div>;
+  const [movieReviews, setMovieReviews] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState('');
+
+  const { movieId } = useParams();
+
+  useEffect(() => {
+    if (!movieId) return;
+
+    async function fetchMovieReviewsById(movieId) {
+      try {
+        setError('');
+        setIsLoading(true);
+        const { results } = await getMovieReviews(movieId);
+        setMovieReviews(results);
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+
+    fetchMovieReviewsById(movieId);
+  }, [movieId]);
+
+  return (
+    <div>
+      {error && <p>{error}</p>}
+      {isLoading && <p>Wait a few moments. Loading...</p>}
+
+      <ul>
+        {movieReviews.map(review => {
+          return (
+            <li key={review.id}>
+              <h3>Author: {review.author}</h3>
+              <p>{review.content}</p>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
 };
 
 export default MovieReviews;
