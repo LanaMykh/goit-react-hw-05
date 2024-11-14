44fadb00364ed8b0b83d8ef518c36f9d21132bc0
Svitlana Mykhailiuk <svetiklan@gmail.com>
2024-11-14 19:36:45 +0200

    lazy

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
 import { Route, Routes } from 'react-router-dom';
-import HomePage from './pages/HomePage';
-import MoviesPage from './pages/MoviesPage';
-import NotFoundPage from './pages/NotFoundPage';
-import MovieDetailsPage from './pages/MovieDetailsPage/MovieDetailsPage';
-import MovieCast from './components/MovieCast/MovieCast';
-import MovieReviews from './components/MovieReviews/MovieReviews';
+// import HomePage from './pages/HomePage';
+// import MoviesPage from './pages/MoviesPage';
+// import NotFoundPage from './pages/NotFoundPage';
+// import MovieDetailsPage from './pages/MovieDetailsPage/MovieDetailsPage';
+// import MovieCast from './components/MovieCast/MovieCast';
+// import MovieReviews from './components/MovieReviews/MovieReviews';
 import Navigation from './components/Navigation/Navigation';
 import './App.css';
+import { lazy, Suspense } from 'react';
+
+const HomePage = lazy(() => import('./pages/HomePage.jsx'));
+const MoviesPage = lazy(() => import('./pages/MoviesPage.jsx'));
+const NotFoundPage = lazy(() => import('./pages/NotFoundPage.jsx'));
+const MovieDetailsPage = lazy(() =>
+  import('./pages/MovieDetailsPage/MovieDetailsPage.jsx')
+);
+const MovieCast = lazy(() => import('./components/MovieCast/MovieCast.jsx'));
+const MovieReviews = lazy(() =>
+  import('./components/MovieReviews/MovieReviews.jsx')
+);
 
 const App = () => {
   return (
@@ -14,16 +26,21 @@ const App = () => {
       <Navigation />
 
       <main>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/movies" element={<MoviesPage />} />
-          <Route path="/movies/:movieId" element={<MovieDetailsPage />}>
-            <Route path="/movies/:movieId/cast" element={<MovieCast />} />
-            <Route path="/movies/:movieId/reviews" element={<MovieReviews />} />
-          </Route>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/movies" element={<MoviesPage />} />
+            <Route path="/movies/:movieId" element={<MovieDetailsPage />}>
+              <Route path="/movies/:movieId/cast" element={<MovieCast />} />
+              <Route
+                path="/movies/:movieId/reviews"
+                element={<MovieReviews />}
+              />
+            </Route>
 
-          <Route path="*" element={<NotFoundPage />} />
-        </Routes>
+            <Route path="*" element={<NotFoundPage />} />
+          </Routes>
+        </Suspense>
       </main>
     </div>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,20 @@
+import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './index.css';
-import App from './App.jsx';
-import 'modern-normalize';
 import { BrowserRouter } from 'react-router-dom';
 
+import App from './App.jsx';
+
+import 'modern-normalize';
+import './index.css';
+
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <StrictMode>
+    <BrowserRouter
+      future={{
+        v7_startTransition: true,
+      }}
+    >
+      <App />
+    </BrowserRouter>
+  </StrictMode>
 );
